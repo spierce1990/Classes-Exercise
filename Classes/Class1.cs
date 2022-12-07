dec3739da8eb108a446a61e2dad65224376a1b53
Sionylen <sionylenpierce@outlook.com>
2022-12-07 14:51:37 -0600

    more

---
+++ Classes/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        public Car()
+        {
+        }
+
+        public string Make { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+
+        public string Year { get; set; } = string.Empty;
+
+    }
+}
+
+/*  Create a class named Car - make sure it is public
+In the Car class:
+Create a Make property of type string that is public
+Create a Model property of type string that is public
+Create a Year property of type int that is public
+In the Main method:
+Create a new instance of the Car class
+Set values in the properties for the object
+Print the values of each property to the Console
+*/
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -5,6 +5,32 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+
+            Car car1 = new Car();
+            car1.Make = "Pontiac";
+            car1.Model = "G6";
+            car1.Year = "2007";
+
+        Console.WriteLine($"The maker of the car is {car1.Make}");
+        Console.WriteLine($"The maker of the car is {car1.Model}");
+        Console.WriteLine($"The maker of the car is {car1.Year}");
         }
+
+
+      
     }
+
 }
+
+
+/*  Create a class named Car - make sure it is public
+In the Car class:
+Create a Make property of type string that is public
+Create a Model property of type string that is public
+Create a Year property of type int that is public
+In the Main method:
+Create a new instance of the Car class
+Set values in the properties for the object
+Print the values of each property to the Console
+*/
